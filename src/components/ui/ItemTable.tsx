a6c644632feefe16a618add84a658f37becee972
Zixuan Chen <remch183@outlook.com>
2024-01-28 19:25:41 +0800

    ui: table layout

--- src/components/ui/ItemTable.tsx
+++ src/components/ui/ItemTable.tsx
@@ -37,6 +37,7 @@ export const ItemTable = ({ items, totalSize }: Props) => {
         item={item}
         totalSize={totalSize}
         depth={depth}
+        collapsedSet={collapsedSetRef}
       />,
     ];
     if (!collapsedSetRef.current.has(item.id)) {
@@ -54,11 +55,11 @@ export const ItemTable = ({ items, totalSize }: Props) => {
   const flatItem = (item: ItemModel) => flat(item, 0);
   const rows = sortedItems.flatMap(flatItem);
   return (
-    <Table>
+    <Table className="max-w-[1100px]">
       <TableCaption>A list of your recent invoices.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead className="">
+          <TableHead className="w-[120px]">
             <Button onClick={() => setSortBy("shallowSize")}>
               Shallow Size{" "}
               <MaterialSymbolsKeyboardArrowDown
@@ -68,8 +69,8 @@ export const ItemTable = ({ items, totalSize }: Props) => {
               />
             </Button>
           </TableHead>
-          <TableHead>Shallow Ratio </TableHead>
-          <TableHead>
+          <TableHead className="w-[80px]">Shallow Ratio </TableHead>
+          <TableHead className="w-[120px]">
             <Button onClick={() => setSortBy("retainSize")}>
               Retain Size
               <MaterialSymbolsKeyboardArrowDown
@@ -79,7 +80,7 @@ export const ItemTable = ({ items, totalSize }: Props) => {
               />
             </Button>
           </TableHead>
-          <TableHead>Retain Ratio</TableHead>
+          <TableHead className="w-[80px]">Retain Ratio</TableHead>
           <TableHead>Name</TableHead>
         </TableRow>
       </TableHeader>
@@ -88,11 +89,14 @@ export const ItemTable = ({ items, totalSize }: Props) => {
   );
 };
 
-const extractFoldState = (item: ItemModel): "none" | "folded" | "expanded" => {
+const extractFoldState = (
+  item: ItemModel,
+  collapsedSet: Set<bigint>,
+): "none" | "folded" | "expanded" => {
   if (item.children.length === 0) {
     return "none";
   }
-  if (item.collapse) {
+  if (collapsedSet.has(item.id)) {
     return "folded";
   }
   return "expanded";
@@ -103,31 +107,46 @@ function Row({
   totalSize,
   depth,
   onClick,
+  collapsedSet,
 }: {
   item: ItemModel;
   totalSize: number;
   depth: number;
   onClick: () => void;
+  collapsedSet: { current: Set<bigint> };
 }) {
-  const foldState = extractFoldState(item);
+  const foldState = extractFoldState(item, collapsedSet.current);
   let icon: React.ReactNode;
   if (foldState === "none") {
-    icon = <span style={{ paddingLeft: depth * 24 }}>📄</span>;
+    icon = <span className="mr-5" style={{ paddingLeft: depth * 24 }}></span>;
   } else if (foldState === "folded") {
-    icon = <span style={{ paddingLeft: depth * 24 }}>📁</span>;
+    icon = (
+      <span className="mr-1 text-gray-400" style={{ paddingLeft: depth * 24 }}>
+        <MaterialSymbolsKeyboardArrowDown className="-rotate-90" />
+      </span>
+    );
   } else {
-    icon = <span style={{ paddingLeft: depth * 24 }}>📂</span>;
+    icon = (
+      <span className="mr-1 text-gray-400" style={{ paddingLeft: depth * 24 }}>
+        <MaterialSymbolsKeyboardArrowDown />
+      </span>
+    );
   }
 
   return (
     <TableRow>
-      <TableCell className="font-medium">{item.shallowSize}</TableCell>
-      <TableCell>
+      <TableCell className="py-1">{item.shallowSize}</TableCell>
+      <TableCell className="py-1">
         {((item.shallowSize / totalSize) * 100).toFixed(2)}%
       </TableCell>
-      <TableCell>{item.retainSize}</TableCell>
-      <TableCell>{((item.retainSize / totalSize) * 100).toFixed(2)}%</TableCell>
-      <TableCell onClick={onClick} className="cursor-pointer">
+      <TableCell className="py-1">{item.retainSize}</TableCell>
+      <TableCell className="py-1">
+        {((item.retainSize / totalSize) * 100).toFixed(2)}%
+      </TableCell>
+      <TableCell
+        onClick={onClick}
+        className="cursor-pointer select-none whitespace-nowrap py-1"
+      >
         {icon}
         <span>{item.name}</span>
       </TableCell>
@@ -154,8 +173,8 @@ export function MaterialSymbolsKeyboardArrowDown(
       width="1em"
       height="1em"
       viewBox="0 0 24 24"
-      className="inline-block"
       {...props}
+      style={{ display: "inline-block", ...props.style }}
     >
       <path
         fill="currentColor"
